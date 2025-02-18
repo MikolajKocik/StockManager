c69ad2f66b882bbda9f19beb6d69fddb9a689152
Mikołaj Kocik <mikolajkocik@onet.pl>
2025-02-18 23:03:31 +0100

    ConnectionWithAPI

---
+++ WinForms/StockManager/API/ApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace StockManager.API
+{
+    public abstract class ApiClient 
+    {
+        protected private readonly HttpClient _httpClient;
+        protected private readonly string _apiBaseUrl;
+        public ApiClient(HttpClient httpClient) 
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _apiBaseUrl = ConfigurationManager.AppSettings["StockManagerAPI:BaseUrl"] ??
+                throw new Exception("BaseUrl is missing in config.");
+        }
+    }
+}

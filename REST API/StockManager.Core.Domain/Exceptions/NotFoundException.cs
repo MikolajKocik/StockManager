763d125814e8c9fbde66990e6d97562cd99ee3d9
Mikołaj <mikolajkocik@onet.pl>
2025-02-22 22:50:04 +0100

    Middleware

---
+++ REST API/StockManager.Core.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManager.Core.Domain.Exceptions
+{
+    public class NotFoundException(string resourceType, string resourceIdentifier) 
+        : Exception("{resourceType} with id: {resourceIdentifier} doesn't exist");
+}
--- REST API/StockManager/Program.cs
+++ REST API/StockManager/Program.cs
@@ -3,6 +3,7 @@ using Serilog;
 using StockManager.Application.Extensions;
 using StockManager.Infrastructure.Data;
 using StockManager.Infrastructure.Extensions;
+using StockManager.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
+
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 //serilog
 builder.Host.UseSerilog((context, configuration) =>
@@ -22,6 +25,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 { 
     app.UseSwagger();

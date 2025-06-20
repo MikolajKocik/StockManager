6113b04f48a9e0e958e16d3bcca3eae0b724a1e8
Mikołaj Kocik <mikolajkocik@onet.pl>
2025-06-20 19:07:52 +0200

    editor missing, result null method

--- REST API/StockManager.Application/Common/ResultPattern/Result.cs
+++ REST API/StockManager.Application/Common/ResultPattern/Result.cs
@@ -1,43 +1,52 @@
-﻿namespace StockManager.Application.Common.ResultPattern
+﻿namespace StockManager.Application.Common.ResultPattern;
+
+public sealed class Result<T> : IResult
 {
-    public sealed class Result<T> : IResult
+    // Represents successfull action
+    public Result(T value)
     {
-        // Represents successfull action
-        public Result(T value)
+        Value = value;
+        Error = null;
+    }
+
+    // Represents error action
+    public Result(Error error)
+    {
+        Error = error;
+        Value = default;
+    }
+
+    public T? Value { get; }
+    public Error? Error { get; }
+    public bool IsSuccess => Error == null;
+
+
+    // method depends on success constructor
+    public static Result<T> Success(T value) => new Result<T>(value);
+
+    // method depends on failure/error constructor
+    public static Result<T> Failure(Error error) => new Result<T>(error);
+
+    /// <summary>
+    /// Transforms the current Result into another type based on whether it is a success or failure.
+    /// </summary>
+    /// <typeparam name="TResult">The type to map to.</typeparam>
+    /// <param name="onSuccess">Function to apply if the result is successful.</param>
+    /// <param name="onFailure">Function to apply if the result is a failure.</param>
+    /// <returns>The result of either the success or failure function.</returns>
+
+    public TResult Map<TResult>(Func<T, TResult> onSuccess, Func<Error, TResult> onFailure) 
+    {
+        ThrowIfAnyNull(onSuccess, onFailure);
+
+        return IsSuccess ? onSuccess(Value!) : onFailure(Error!);
+    }
+
+    private static void ThrowIfAnyNull(params object[] args)
+    {
+        foreach(object arg in args)
         {
-            Value = value;
-            Error = null;
-        }
-
-        // Represents error action
-        public Result(Error error)
-        {
-            Error = error;
-            Value = default;
-        }
-
-        public T? Value { get; }
-        public Error? Error { get; }
-        public bool IsSuccess => Error == null;
-
-
-        // method depends on success constructor
-        public static Result<T> Success(T value) => new Result<T>(value);
-
-        // method depends on failure/error constructor
-        public static Result<T> Failure(Error error) => new Result<T>(error);
-
-        /// <summary>
-        /// Transforms the current Result into another type based on whether it is a success or failure.
-        /// </summary>
-        /// <typeparam name="TResult">The type to map to.</typeparam>
-        /// <param name="onSuccess">Function to apply if the result is successful.</param>
-        /// <param name="onFailure">Function to apply if the result is a failure.</param>
-        /// <returns>The result of either the success or failure function.</returns>
-
-        public TResult Map<TResult>(Func<T, TResult> onSuccess, Func<Error, TResult> onFailure)
-        {
-            return IsSuccess ? onSuccess(Value!) : onFailure(Error!);
+            ArgumentNullException.ThrowIfNull(arg);
         }
     }
 }

734aff1df9b9d84285aaa345df153064d26faaf3
MikoŁaj Kocik <mikolajkocik@onet.pl>
2026-03-24 19:47:47 +0100

    feat: add modal for create product action

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductList from './pages/products/ProductList';
 import ProductDetails from './pages/products/ProductDetails';
-import ProductCreateForm from './pages/products/ProductCreateForm';
 import Layout from './components/Layout';
 import ProductEditForm from './pages/products/ProductEditForm';
 
@@ -12,7 +11,6 @@ export default function App() {
           <Route element={<Layout />}>
             <Route path="/" />
             <Route path="/products" element={<ProductList />} />
-            <Route path="/products/create" element={<ProductCreateForm />} />
             <Route path="/products/:id" element={<ProductDetails />} />
             <Route path="/products/edit/:id" element={<ProductEditForm />} />
           </Route>       
--- Frontend/src/pages/products/ProductCreateForm.tsx
+++ Frontend/src/pages/products/ProductCreateForm.tsx
@@ -1,8 +1,14 @@
 import type { ProductCreateForm } from "../../models/product";
 import { useState, useEffect } from 'react';
 import api from '../../api/api';
+import Modal from '../../components/Modal';
  
-export default function ProductCreateForm() {
+interface ProductCreateModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export default function ProductCreateForm({ isOpen, onClose }: ProductCreateModalProps) {
     const [genres, setGenres] = useState<string[]>([]);
     const [types, setTypes] = useState<string[]>([]);
     const [error, setError] = useState<string | null>(null);
@@ -27,11 +33,19 @@ export default function ProductCreateForm() {
 
     const handleSubmit = async (e: React.SyntheticEvent) => {
         e.preventDefault();
-        await api.post('/products', form);
+        try {
+            await api.post('/products', form);
+            onClose();
+        } catch (err) {
+            setError("Error occurred while saving data...");
+        }
     };
 
     useEffect(() => {
+        if (!isOpen) return;
+
         const fetchData = async () => {
+            setLoading(true);
             try {
                 const [genresRes, typesRes] = await Promise.all([
                     api.get<string[]>('/products/genres'),
@@ -49,46 +63,53 @@ export default function ProductCreateForm() {
         }
 
         fetchData();
-    }, []);
+    }, [isOpen]);
     
-    if (loading) return <p>Loading...</p>;
-
-    if (error) return <p style={{ color: 'red'}}>{error}</p>;
-
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <input name="name" type="text" value={form.name} onChange={handleChange} />
-            </div>
-            <div>
-                <select name="genre" value={form.genre} onChange={handleSelectChange}>
-                    {genres.map(g => (
-                        <option key={g} value={g}>{g}</option>
-                    ))}
-                </select>
-            </div>
-            <div>
-                <input name="unit" type="text" value={form.unit} onChange={handleChange} />
-            </div>
-            <div>
-                <select name="type" value={form.type} onChange={handleSelectChange}>
-                    {types.map(t => (
-                        <option key={t} value={t}>{t}</option>
-                    ))}
-                </select>
-            </div>
-            <div>
-                <input name="batchNumber" type="text" value={form.batchNumber} onChange={handleChange} />
-            </div>
-            <div>
-                <input name="supplierId" type="text" value={form.supplierId} onChange={handleChange} />
-            </div>
-            <div>
-                <input name="expirationDate" type="date" value={form.expirationDate} onChange={handleChange} />
-            </div>
-            <div>
-                <button type="submit">Add product</button>
-            </div>
-        </form>
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <h2>Add new product</h2>
+
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p style={{ color: 'red'}}>{error}</p>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input name="name" type="text" value={form.name} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <select name="genre" value={form.genre} onChange={handleSelectChange}>
+                            {genres.map(g => (
+                                <option key={g} value={g}>{g}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <input name="unit" type="text" value={form.unit} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <select name="type" value={form.type} onChange={handleSelectChange}>
+                            {types.map(t => (
+                                <option key={t} value={t}>{t}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <input name="batchNumber" type="text" value={form.batchNumber} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <input name="supplierId" type="text" value={form.supplierId} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <input name="expirationDate" type="date" value={form.expirationDate} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <button type="submit">Add product</button>
+                        <button type="button" onClick={onClose}>Cancel</button>
+                    </div>
+                </form>
+            )}
+        </Modal>
     )
 }

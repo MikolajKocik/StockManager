e80fb6a0f7a5629275a7ab40131d436f11ae8424
MikoŁaj Kocik <mikolajkocik@onet.pl>
2026-04-23 16:59:08 +0200

    chore: make city, postalcode, country address props as nullable

--- REST API/StockManager.Application/Dtos/ModelsDto/AddressDtos/AddressDto.cs
+++ REST API/StockManager.Application/Dtos/ModelsDto/AddressDtos/AddressDto.cs
@@ -3,9 +3,9 @@
 public sealed record AddressDto
 {
     public Guid Id { get; init; }
-    public string City { get; init; }
-    public string Country { get; init; }
-    public string PostalCode { get; init; }
+    public string? City { get; init; }
+    public string? Country { get; init; }
+    public string? PostalCode { get; init; }
     public Guid SupplierId { get; init; }
 }
 

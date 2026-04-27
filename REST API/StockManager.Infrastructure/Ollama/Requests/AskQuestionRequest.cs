18fd9e104fbd148bdd3aebf59dae07ae4cd60b9a
MikoŁaj Kocik <mikolajkocik@onet.pl>
2026-04-27 15:32:02 +0200

    feat: update ollama based question request

--- REST API/StockManager.Infrastructure/Ollama/Requests/AskQuestionRequest.cs
+++ REST API/StockManager.Infrastructure/Ollama/Requests/AskQuestionRequest.cs
@@ -2,13 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace StockManager.Infrastructure.Ollama.Requests;
 
-public class AskQuestionRequest
+public sealed record AskQuestionRequest
 {
     [Required(ErrorMessage = "Question cant be a zero value.")]
     [MaxLength(1000, ErrorMessage = "Question is too long.")]
-    public required string Question { get; set; }
+    public required string Question { get; init; }
 
-    public Guid? ConversationId { get; set; }
+    public Guid? ConversationId { get; init; }
 
-    public string? CategoryFilter { get; set; }
+    public string? CategoryFilter { get; init; }
+    public string? WarehouseFilter { get; init; }
 }
